d62b3d6ea3f0730ab045c755baa52edcafd22322
遠山聡美 <S083@inv-238.inv.local>
2017-12-28 12:33:16 +0900

    Initial Commit

---
+++ SampleQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleQuiz
+//
+//  Created by 遠山　聡美 on 2017/12/28.
+//  Copyright © 2017年 Simple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
