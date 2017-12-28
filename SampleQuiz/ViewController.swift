3ce73ad399191cd05f4b7a347994547209c72fa5
遠山聡美 <S083@inv-238.inv.local>
2017-12-28 19:45:25 +0900

    音とバイブレーションを設定。完成

--- SampleQuiz/ViewController.swift
+++ SampleQuiz/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    @IBAction func tapSystemSound(_ sender: Any) {
+        AudioServicesPlayAlertSoundWithCompletion(1027) {
+            //サウンドが鳴り終わった後に行う処理
+        }
+    }
+    
+    @IBAction func tapCastomSound(_ sender: Any) {
+        //指定するサウンドファイルの再生時間が30秒以下であること
+        let soundUrl = Bundle.main.url(forResource: "taiko01", withExtension: "mp3")
+        
+        //サウンドIDを登録するための変数を準備
+        var soundId: SystemSoundID = 0
+        //サウンドファイルを登録してサウンドIDを取得 「&」は参照渡し
+        AudioServicesCreateSystemSoundID(soundUrl as! CFURL, &soundId)
+        AudioServicesPlayAlertSoundWithCompletion(soundId){
+            //サウンドが鳴り終わった後に行う処理
+        }
+    }
+    
+    @IBAction func tapVibration(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate)  {
+            //バイブレーションがなり終わった後におこなう処理
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
